ae4d7ae78d685ef27611fde64c37811e0b8ed497
Ousama Mohammad <osama03mohammad@gmail.com>
2026-05-14 20:21:47 +0300

    installed react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import "./App.css";
 import { HomePage } from "./pages/HomePage";
+import { Routes, Route } from "react-router";
 
 function App() {
-  return <HomePage />;
+  return (
+    <Routes>
+      {/* for shortCut for the "/" we can use "index" */}
+      <Route index element={<HomePage />} />
+      <Route path="checkout" element={}/>
+    </Routes>
+  );
 }
 
 export default App;
